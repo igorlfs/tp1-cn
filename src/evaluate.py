addba95f6f80500977edd7e8497baf6e5efe082d
Igor <igorlfs@ufmg.br>
2024-10-31 00:54:43 -0300

    perf(evaluate): massive speedup by not being stupid

--- src/evaluate.py
+++ src/evaluate.py
@@ -9,19 +9,20 @@ from src.tree import TreeNode
 model = AgglomerativeClustering(metric="precomputed", linkage="average")
 
 
-def evaluate_fitness(x_train: pd.DataFrame, tree: TreeNode, y_train: NDArray) -> float:
-    distance_matrix_train = _get_distance_matrix(x_train, tree)
+def evaluate_fitness(x: pd.DataFrame, tree: TreeNode, y: NDArray) -> float:
+    evaluations = [tree.evaluate(row.to_dict()) for _, row in x.iterrows()]
+    distance_matrix_train = _get_distance_matrix(len(x), evaluations)
     model.fit(distance_matrix_train)
     y_pred = model.fit_predict(distance_matrix_train)
-    return float(v_measure_score(y_train, y_pred))
+    return float(v_measure_score(y, y_pred))
 
 
-def _get_distance_matrix(x: pd.DataFrame, tree: TreeNode) -> NDArray[np.float64]:
-    distance_matrix: NDArray[np.float64] = np.zeros(shape=(len(x), len(x)))
+def _get_distance_matrix(n: int, evaluations: list[float]) -> NDArray[np.float64]:
+    distance_matrix: NDArray[np.float64] = np.zeros(shape=(n, n))
     # TODO fazer a diferença antes ou depois?
-    for i, row1 in x.iterrows():
-        eval1 = tree.evaluate(row1.to_dict())
-        for j, row2 in x.iterrows():
-            eval2 = tree.evaluate(row2.to_dict())
-            distance_matrix[i, j] = abs(eval1 - eval2)
+    for i in range(n):
+        for j in range(i, n):
+            distance = abs(evaluations[i] - evaluations[j])
+            distance_matrix[i, j] = distance
+            distance_matrix[j, i] = distance
     return distance_matrix
