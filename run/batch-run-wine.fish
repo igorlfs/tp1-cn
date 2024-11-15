b3cd1cb9b5c1f0cb6cac0c61010a4014ce98662d
Igor <igorlfs@ufmg.br>
2024-11-15 20:58:15 -0300

    missing batch run wine

---
+++ run/batch-run-wine.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/env fish
+
+set NUMBER_OF_REPETITIONS 10
+
+set DEFAULT_ARGS --swap-terminal-probability 0.5 --swap-operator-probability 0.5 --leaf-probability 0.1 --crossover-probability 0.9 --mutation-probability 0.3 --tournament-size 2 --elitism-size 1 --max-generations 100 --population-size 100 --max-depth 7 --verbose wine_red
+
+set LOG_DIR "./dumps"
+
+set BASE_NAME "wine"
+
+set SHELL_SEED 634441
+
+random $SHELL_SEED
+
+set SEED_MIN 100000
+set SEED_MAX 999999
+
+for j in (seq 1 $NUMBER_OF_REPETITIONS)
+    set SEED (random $SEED_MIN $SEED_MAX)
+    set ARGS $DEFAULT_ARGS --seed $SEED
+    uv run python -m src $ARGS > "$LOG_DIR/$i-$(math $j - 1)-$BASE_NAME.csv"
+end
+uv run python scripts/extract_statistics.py --path $LOG_DIR --name $BASE_NAME --iter $NUMBER_OF_REPETITIONS --var $i
